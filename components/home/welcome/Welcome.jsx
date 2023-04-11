25fe234d34ee190ac9a3b24c0a119813d3d6c903
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2023-04-11 13:58:40 -0700

    added built in style props

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { View, Text } from 'react-native'
 
-import styles from './welcome.style'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
+import { useRouter } from 'expo-router';
+
+import styles from './welcome.style';
+import {icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello T-Dog</Text>
+
+      </View>
     </View>
   )
 }
