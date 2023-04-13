f57f92a32c49992fda8cc57f12804467bd97e26f
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2023-04-13 11:01:36 -0700

    job -card built out with key-extractor

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -8,10 +8,39 @@ import { COLORS, SIZES } from '../../../constants'
 import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 
 const Popularjobs = () => {
+
+  const isLoading = false;
+  const error = false
   const router = useRouter();
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popularjobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View> 
+
+      <View style={styles.cardsContainer}>
+        {isLoading ?(
+          <ActivityIndicator size='large'  colors={COLORS.primary}/>
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList 
+          data={[1,2,3,4]}
+          renderItem={({item}) => (
+            <PopularJobCard 
+            item={item}
+            />
+          )}
+          keyExtractor={item => item?.job_id}
+          contentContainerStyle={{ columnGap: SIZES.medium }}
+          horizontal
+          />
+        )
+      }
+      </View>
     </View>
   )
 }
