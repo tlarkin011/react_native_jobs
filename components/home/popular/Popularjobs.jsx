9c66b73af11a50397e25cde9e943c8b001ef7e44
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2023-04-17 15:07:44 -0700

    fixed bug with card style passed in (item)

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -13,10 +13,10 @@ const Popularjobs = () => {
   const { data, isLoading, error } = useFetch
   ('search', {
     query: 'React developer',
-    num_pages: 1
+    num_pages: "1",
   })
 
-  console.log(data)
+  
   const router = useRouter();
   return (
     <View style={styles.container}>
@@ -34,13 +34,13 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList 
-          data={[1,2,3,4]}
+          data={data}
           renderItem={({item}) => (
             <PopularJobCard 
             item={item}
             />
           )}
-          keyExtractor={item => item?.job_id}
+          keyExtractor={(item) => item?.job_id}
           contentContainerStyle={{ columnGap: SIZES.medium }}
           horizontal
           />
