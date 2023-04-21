b0c99feacc3ad7fcd6b3284eac824fc931bafa5a
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2023-04-21 15:25:56 -0700

    added routing

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -14,7 +14,7 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
         <Image
           source={{
             uri: checkImageURL(job.employer_logo)
-              ? item.employer_logo
+              ? job.employer_logo
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
           resizeMode='contain'
